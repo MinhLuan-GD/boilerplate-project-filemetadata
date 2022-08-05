9162efc37ef1b179f7941a271e24e67db833c7c1
Nguyen Duy Minh Luan <luannguyen11998@gmail.com>
2022-08-05 19:20:09 +0700

    finished

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var cors = require("cors");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 require("dotenv").config();
 
 var app = express();
@@ -10,6 +12,11 @@ app.use("/public", express.static(process.cwd() + "/public"));
 
 app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  const { originalname, mimetype, size } = req.file;
+  res.json({ name: originalname, type: mimetype, size });
 });
 
 app.listen(port, function () {
